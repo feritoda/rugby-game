c767dca06ef64e28a715dcf12a437e1ec75322e9
feritoda <55368101+feritoda@users.noreply.github.com>
2022-04-01 09:42:06 -0300

    Add files via upload

--- src/attacker.c
+++ src/attacker.c
@@ -12,6 +12,14 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+static int turn = 0;
+
+/*----------------------------------------------------------------------------*/
+/*                          PRIVATE FUNCTIONS HEADERS                         */
+/*----------------------------------------------------------------------------*/
+
+bool equal_directions (direction_t d1, direction_t d2);
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
@@ -23,7 +31,40 @@ direction_t execute_attacker_strategy(
   UNUSED(defender_spy);
 
   // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  static direction_t best_direction;
+  static position_t previous_position;
+  
+  if (turn == 0){
+    best_direction = (direction_t) DIR_DOWN_RIGHT;
+    previous_position = (position_t) INVALID_POSITION;
+  }
+
+  if (equal_positions (previous_position, attacker_position)){
+    if (equal_directions (best_direction, (direction_t) DIR_DOWN_RIGHT) ||
+        equal_directions (best_direction, (direction_t) DIR_RIGHT)) {
+          if (equal_directions (best_direction, (direction_t) DIR_DOWN_RIGHT))
+            best_direction = (direction_t) DIR_RIGHT;
+          else
+            best_direction = (direction_t) DIR_UP;
+    }
+
+    else 
+      best_direction = (direction_t) DIR_DOWN_RIGHT;
+  }
+    
+      
+  turn++;
+  previous_position = attacker_position;
+  return best_direction;
+}
+
+
+/*----------------------------------------------------------------------------*/
+/*                             PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+bool equal_directions (direction_t d1, direction_t d2){
+  return d1.i == d2.i && d1.j == d2.j;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -12,6 +12,14 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+static int turn = 0;
+
+/*----------------------------------------------------------------------------*/
+/*                          PRIVATE FUNCTIONS HEADERS                         */
+/*----------------------------------------------------------------------------*/
+
+bool equal_directions2(direction_t d1, direction_t d2);
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
@@ -23,7 +31,55 @@ direction_t execute_defender_strategy(
   UNUSED(attacker_spy);
 
   // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  static direction_t best_direction;
+  static position_t attacker_position;
+  static int distance; 
+
+  if (turn == 0)
+    best_direction = (direction_t) DIR_LEFT;
+
+  if (turn == 1){
+    attacker_position = get_spy_position(attacker_spy);
+    
+    distance = defender_position.j - attacker_position.j;
+
+    if (attacker_position.i < defender_position.i)
+      best_direction = (direction_t) DIR_UP;
+    
+    else if (attacker_position.i > defender_position.i)
+      best_direction = (direction_t) DIR_DOWN;
+  }
+
+  if (equal_directions2(best_direction, (direction_t) DIR_DOWN) ||
+      equal_directions2(best_direction, (direction_t) DIR_DOWN_LEFT)){
+    if (turn - 1 <= distance / 2)
+      best_direction = (direction_t) DIR_DOWN_LEFT;
+    else
+      best_direction = (direction_t) DIR_STAY;
+  }
+
+  else if (equal_directions2(best_direction, (direction_t) DIR_UP) ||
+            equal_directions2(best_direction, (direction_t) DIR_UP_LEFT)){
+    if (turn - 1 <= distance / 2)
+      best_direction = (direction_t) DIR_UP_LEFT;
+    else
+      best_direction = (direction_t) DIR_STAY;
+  
+  }
+    
+      
+  turn++;
+  return best_direction;
+
+
+}
+
+/*----------------------------------------------------------------------------*/
+/*                             PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+bool equal_directions2 (direction_t d1, direction_t d2){
+  return d1.i == d2.i && d1.j == d2.j;
 }
 
 /*----------------------------------------------------------------------------*/
